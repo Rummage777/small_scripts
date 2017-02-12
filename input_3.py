9c684e69b0f96a069ad50a01e343647d244751ea
Rummage777 <777.free.writer@gmail.com>
2017-02-12 02:14:23 +0200

    Rewrite loop and conditions

--- input_3.py
+++ input_3.py
@@ -4,15 +4,15 @@
 # Перепишите предыдущую программу так, чтобы пользователю
 # предлагалось решать пример до тех пор, пока он не напишет
 # правильный ответ. (При решении задачи используйте цикл while.)
+answer = None
+print 'Solve the problem: 4*100 - 54. '
 
-answer = raw_input('Solve the problem: 4*100 - 54. ')
-while answer.isdigit() is False:
-    print "Wrong input. You should print only digits"
-    answer = raw_input('Print the answer  ')
-
-answer = int(answer)
 while answer != 346:
-    print "Answer is incorrect. Try again!"
     answer = raw_input('Print the answer  ')
-    answer = int(answer)
+    if answer.isdigit() is False:
+        print "Wrong input. You should print only digits"
+    else:
+        answer = int(answer)
+        if answer != 346:
+            print "Answer is incorrect. Try again!"
 print "You are brilliant! Answer is correct!"
